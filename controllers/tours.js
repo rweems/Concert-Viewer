78f270537a41904fe22c58c3880020107057aa0d
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-04-25 19:09:17 -0400

    added updated

--- controllers/tours.js
+++ controllers/tours.js
@@ -13,8 +13,17 @@ const tourController = {
     new: (req,res) => {
         res.render('concert/createTour');
     },
-
-    update:{},
+    edit: (req,res) => {
+        tour.findById(req.params.id).then(tour => {
+            res.render('concert/edit', {tour});
+        })
+    },
+    update:(req,res) => {
+        //find by id, update the body, return modified document
+        tour.findByIdAndUpdate(req.params.id, req.body, {new:true}).then(() => {
+            res.redirect(`${req.params.id}`)
+        })
+    },
     delete:{}
 }
 
