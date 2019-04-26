aea008910430b275bc9f7be93980aa2a00494577
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-04-26 09:25:00 -0400

    ...

--- db/connection.js
+++ db/connection.js
@@ -2,11 +2,19 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect("mongodb://localhost/concert-viewer", {useNewUrlParser: true})
-    .then(() => {
-        console.log("Mongodb connected");
-    });
-
-
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+  }
+  else {
+    mongoose.connect('mongodb://localhost/express-movies');
+  }
+  mongoose.connection.on('error', function(err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+    }
+  );
+  mongoose.connection.once('open', function() {
+    console.log("Mongoose has connected to MongoDB!");
+  });
 
     module.exports = mongoose;
