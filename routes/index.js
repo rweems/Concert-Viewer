e8f646156b23712baeb45f75f1539cdb70944596
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-04-26 10:09:55 -0400

    fixes #1

--- routes/index.js
+++ routes/index.js
@@ -14,5 +14,7 @@ router.get('/:id/edit', tourController.edit);
 
 router.put('/:id', tourController.update);
 
+router.get('/:id',tourController.show);
+
 router.put('/:id', tourController.update);
 module.exports = router;
